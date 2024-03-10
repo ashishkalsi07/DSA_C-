7e58d8a134db2af5035a7b50c8eff69925880b0e
Ashish Kalsi <68415917+ashishkalsi07@users.noreply.github.com>
2024-03-10 15:04:59 +0530

    Kadane's Algorithm.cpp

--- Kadane's Algorithm.cpp
+++ Kadane's Algorithm.cpp
@@ -1,2 +1,21 @@
 The name itself has so much importance in Arrays ,that it does not need any introduction to the problem Still:
 We need to find max sub array sum with the time complexity of O(n)  , Let see now
+#include <iostream>
+using namespace std;
+int main() {
+  int sum = 2;
+  int arr[]={5,-3,1,-6,5,3};
+  int n=sizeof(arr)/sizeof(arr[0]);
+  int maximum=arr[0];
+  for(int i=0;i<n;i++)
+  {
+    sum += arr[i];
+    if(sum > maximum){
+      maximum = sum;
+    }
+    if(sum < 0){
+      sum = 0;
+    }
+  }
+  cout<< maximum;
+}
